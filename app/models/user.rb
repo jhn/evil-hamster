4c27d026fbdcc8cb14380e88b2d9071fd38f783c
Johan Mena <johan.menac@gmail.com>
2015-09-20 21:16:10 -0400

    HW2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,6 @@
 class User
   include Mongoid::Document
-
-  field :first_name,   :type => String
-  field :last_name,    :type => String
-  field :age,          :type => Integer
-  field :email,        :type => String
-  field :phone_number, :type => String
-  field :birthday,     :type => String
-
+  field :first_name, type: String
+  field :last_name, type: String
+  field :email, type: String
 end
